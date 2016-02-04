3d6735f768c032233fa7190e717fd6335891b865
JaviBP <javifocus88@hotmail.com>
2016-02-04 22:10:21 +0100

    Menu terminado

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,18 @@
 /**
  * Created by anonimo1 on 02/02/2016.
  */
+$(document).ready()
+{
+    var a = false;
+    $('.submenu').click(function(){
+        if(a==false) {
+            $(this).children('ul').stop(true, true).slideDown('slow');
+            a = true;
+            $(this).animate({ paddingLeft: '-150px' }, 200);
+        }else {
+            a=false;
+            $(this).children('ul').stop(true, true).slideUp('slow');
+            $(this).animate({ paddingLeft: 0 }, 200);
+        }
+    });
+}
